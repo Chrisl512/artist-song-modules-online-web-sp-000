7f5726e7c3d7ba7d0cf14322dfca858f89b167d5
Christopher Luna <chris@illusionlayervfx.com>
2019-08-31 03:56:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,8 @@ require_relative '../lib/concerns/memorable'
 
 class Artist
 
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   include Paramable
 
   attr_accessor :name
